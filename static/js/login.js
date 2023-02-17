50e868899ec3bda53910731bd1291ee3a8804c6d
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-17 16:22:03 -0400

    arreglando estilos de formulario y creando archivo para validacion por js

---
+++ static/js/login.js
@@ -0,0 +1,63 @@
+const inputs = document.querySelectorAll('#formulario input'); //declarando una constante con todos los inputs dentro de la id formulario
+
+const expresiones = { //objeto con varias expresiones regulares
+ 
+  nombre: /^[a-zA-ZÀ-ÿ\s]{3,20}$/,
+  //expresion regular de codigo de usuario
+}
+
+const ValidarFormulario = (e) => {
+  switch (e.target.name) {
+  
+    case "nombre":
+      ValidarCampo(expresiones.nombre, e.target, 'nombre');
+      break;
+
+  }
+}
+const ValidarCampo = (expresion, input, campo) => {
+  if (expresion.test(input.value)) {
+    document.querySelector(`#grupo__${campo} i`).classList.remove('bi', 'bi-exclamation-triangle-fill', 'text-danger', 'input-icon');
+    document.querySelector(`#grupo__${campo} p`).classList.remove('d-block');
+    document.querySelector(`#grupo__${campo} i`).classList.add('bi', 'bi-check-circle-fill', 'text-check', 'input-icon2');
+    document.querySelector(`#grupo__${campo} p`).classList.add('d-none');
+    campos[campo] = true;
+  } else {
+    document.querySelector(`#grupo__${campo} i`).classList.remove('bi', 'bi-check-circle-fill', 'text-check', 'input-icon2');
+    document.querySelector(`#grupo__${campo} p`).classList.remove('d-none');
+    document.querySelector(`#grupo__${campo} i`).classList.add('bi', 'bi-exclamation-triangle-fill', 'text-danger', 'input-icon');
+    document.querySelector(`#grupo__${campo} p`).classList.add('d-block');
+    campos[campo] = false;
+  }
+}
+
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', ValidarFormulario);
+  input.addEventListener('blur', ValidarFormulario);
+});
+
+
+formulario.addEventListener('submit', (e) => {
+  if (!(campos.nombre )) {
+    e.preventDefault();
+    Swal.fire({
+      icon: 'error',
+      title: 'Lo siento ',
+      text: 'No puedes entrar sin un nombre de jugador'
+    })
+  }
+})
+
+
+
+if (error == false) { 
+  Swal.fire({
+    icon: 'success',
+    title: 'Te has unido al juego correctamente'
+  })
+  const myTimeout = setTimeout(redireccion, 2000);
+}
+  function redireccion() {
+    window.location = "index.php?pagina=zona_espera.php";
+  }
--- vista/login.php
+++ vista/login.php
@@ -4,9 +4,57 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Ingresar como jugador</title>
+  <link rel="stylesheet" href="./static/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./static/css/login.css">
 </head>
-<body>
-  
+<body class="h-100">
+  <div class="container-fluid ">
+  <section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+                <div class="text-center">
+                  <img class="rounded-circle" src="./static/img/control1.jpg"
+                    style="width: 185px;" alt="logo">
+                  <h4 class="mt-1 mb-5 pb-1">¡Bienvenido al juego de pulsar los recuadros!</h4>
+                </div>
+                <form id="formulario">
+                  <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example11">Nombre de jugador</label>
+                    <input type="email" id="form2Example11" class="form-control"
+                      placeholder="Jugadora 1" />
+                    
+                  </div>
+
+                  
+                  <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">¡A jugar!</button>
+                    
+                  </div>
+                 
+    
+                </form>
+
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                <h4 class="mb-4 text-center"> Tutorial </h4>
+                <p class="small mb-0">Este juego se trata de pulsar 3 botones tendras un color predeterminado de jugador, jugaras contra otro usuario (debes esperar a que inicie otro usuario
+                  ). el objetivo sera pintar los 3 colores lo mas rapido posible al lograr los 3 colores mas rapido que tu oponente ganas.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  </div>
 </body>
 </html>
