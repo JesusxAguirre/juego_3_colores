cc486144a5416455d79983490539f33784d7849d
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-18 01:12:17 -0400

    ya funciona la redireccion de usuarios y el cambio de colores, falta que les salga a ambos jugadores

--- src/Chat.php
+++ src/Chat.php
@@ -7,7 +7,7 @@ use Usuarios;
 
 $usuarios= array();
 
-
+$contador = 0;
 class Chat implements MessageComponentInterface {
     protected $clients;
 
@@ -21,33 +21,33 @@ class Chat implements MessageComponentInterface {
     }
   
     public function onOpen(ConnectionInterface $conn) {
-
+      global  $contador;
+      $contador++;
       $this->clients->attach($conn);
       $fecha_actual = date("d-m-Y h:i:s");
    
 
-      if(count($this->clients) >= 2){
-        $background_colors = array('#282E33', '#25373A', '#164852', '#495E67', '#FF3838');
+      if($contador >= 3){
+        $background_colors = array('#AEF520', '#2026F5', '#DC1A75', '#DC1A1A', '#1ADC61');
 
         $count = count($background_colors) - 1;
     
         $i = rand(0, $count);
         echo "Entro a el bucle asdasdas ({$conn->resourceId})  \n";
-        foreach($this->clients as $client){
+        
           $usuarios["color"] =   $background_colors[$i];
           $usuarios["id"] = $conn->resourceId;
           $conn->send(json_encode($usuarios));
-          unset($usuarios);
-        }
+        
+       
       }
       echo "Nueva conexion $fecha_actual ({$conn->resourceId})  \n";
     }
     
 
     public function onMessage(ConnectionInterface $from, $msg) {
-      
-    
-   
+      global $usuarios;
+     
 
        $numRecv = count($this->clients) - 1;
        echo sprintf('El usuario %d esta enviando el mensaje: "%s" to %d other connection%s' . "\n"
--- static/js/juego.js
+++ static/js/juego.js
@@ -3,25 +3,28 @@ $(document).ready(function (e) {
   var objeto_websocket = new WebSocket("ws://localhost:8080");
   var nombre_usuario = $("#usuario").val()
   var data
+  var boton_1 = document.getElementById("1")
+  var boton_2 = document.getElementById("2")
+  var boton_3 = document.getElementById("3")
 objeto_websocket.onmessage = function (e) {
-  var data = JSON.parse(e.data);
+  data = JSON.parse(e.data);
   console.log(data)
-  
+
 
 
 }
   
-$("#1").click(function(){
+  $("#1").on('click',function(){
 
-  $("1").css("background-color","" + data.color + "");
+  boton_1.style.background = data.color
+  })
+$("#2").on('click',function(){
+
+ boton_2.style.background = data.color
 })
-$("#2").click(function(){
+$("#3").on('click',function(){
 
-  $("2").css("background-color","" + data.color + "");
-})
-$("#3").click(function(){
-
-  $("3").css("background-color","" + data.color + "");
+  boton_3.style.background = data.color
 })
 
 
